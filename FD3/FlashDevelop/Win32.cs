4644f25a4959612ac0048d8f8d6aac9b6b951316
meychi <meychi@2354be2f-a62e-0410-afb7-89360a396392>
2008-09-12 08:35:42 +0000

    Removed unused Win32 stuff...
    git-svn-id: http://flashdevelop.googlecode.com/svn/trunk@83 2354be2f-a62e-0410-afb7-89360a396392

--- FD3/FlashDevelop/Win32.cs
+++ FD3/FlashDevelop/Win32.cs
@@ -10,19 +10,10 @@ namespace FlashDevelop
         public const Int32 SW_SHOW = 1;
         public const Int32 SW_RESTORE = 9;
         public const Int32 SW_SHOWMINIMIZED = 2;
-        public const Int32 SWP_SHOWWINDOW = 64;
+        public const UInt32 SWP_SHOWWINDOW = 64;
 
-        [DllImport("user32.dll")]
-        public static extern Int32 GetSystemMetrics(Int32 which);
-
-        [DllImport("user32.dll")]
-        public static extern Int32 ShowWindow(Int32 hWnd, Int32 nCmdShow);
-        
         [DllImport("user32.dll")]
         public static extern Boolean ShowWindow(IntPtr hWnd, Int32 nCmdShow);
-
-        [DllImport("user32.dll")]
-        public static extern Int32 FindWindow(String lpszClassName, String lpszWindowName);
 
         [DllImport("user32.dll")]
         public static extern void SetWindowPos(IntPtr hwnd, IntPtr hwndInsertAfter, Int32 x, Int32 y, Int32 width, Int32 height, UInt32 flags);
