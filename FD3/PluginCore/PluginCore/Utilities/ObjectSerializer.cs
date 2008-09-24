fa37419078002bf0cfab765e31594029e3ab4066
meychi <meychi@2354be2f-a62e-0410-afb7-89360a396392>
2008-09-24 09:09:42 +0000

    Try to omit the assembly name error...
    git-svn-id: http://flashdevelop.googlecode.com/svn/trunk@128 2354be2f-a62e-0410-afb7-89360a396392

--- FD3/PluginCore/PluginCore/Utilities/ObjectSerializer.cs
+++ FD3/PluginCore/PluginCore/Utilities/ObjectSerializer.cs
@@ -27,11 +27,17 @@ namespace PluginCore.Utilities
         /// </summary>
         static Assembly CurrentDomainAssemblyResolve(Object sender, ResolveEventArgs args)
         {
-            AssemblyName name = new AssemblyName(args.Name);
-            String ffile = Path.Combine(PathHelper.AppDir, name.Name + ".exe");
-            String afile = Path.Combine(PathHelper.AppDir, name.Name + ".dll");
-            String dfile = Path.Combine(PathHelper.PluginDir, name.Name + ".dll");
-            String ufile = Path.Combine(PathHelper.UserPluginDir, name.Name + ".dll");
+            String shortName = String.Empty;
+            try
+            {
+                AssemblyName assemblyName = new AssemblyName(args.Name);
+                shortName = assemblyName.Name;
+            }
+            catch { shortName = args.Name; }
+            String ffile = Path.Combine(PathHelper.AppDir, shortName + ".exe");
+            String afile = Path.Combine(PathHelper.AppDir, shortName + ".dll");
+            String dfile = Path.Combine(PathHelper.PluginDir, shortName + ".dll");
+            String ufile = Path.Combine(PathHelper.UserPluginDir, shortName + ".dll");
             if (File.Exists(ffile)) return Assembly.LoadFrom(ffile);
             if (File.Exists(afile)) return Assembly.LoadFrom(afile);
             if (File.Exists(dfile)) return Assembly.LoadFrom(dfile);
