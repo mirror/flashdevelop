6fd7b371f2ffa6bdb35d1cf10f3a6f547650ea7d
philippe.elsass <philippe.elsass@2354be2f-a62e-0410-afb7-89360a396392>
2010-01-04 20:26:18 +0000

    - regression in completion
    git-svn-id: http://flashdevelop.googlecode.com/svn/trunk@819 2354be2f-a62e-0410-afb7-89360a396392

--- FD3/External/Plugins/ASCompletion/Completion/ASComplete.cs
+++ FD3/External/Plugins/ASCompletion/Completion/ASComplete.cs
@@ -2644,7 +2644,8 @@ namespace ASCompletion.Completion
         /// </summary>
         static public bool IsTextStyle(int style)
 		{
-			return style == 11 /*identifier*/ || style == 16 /*word2 (secondary keywords: class name)*/
+			return style == 10 /*punctuation*/ || style == 11 /*identifier*/ 
+                || style == 16 /*word2 (secondary keywords: class name)*/
                 || style == 21 /*word4 (add keywords4)*/ || style == 22 /*word5 (add keywords5)*/;
 		}
 
@@ -2654,7 +2655,8 @@ namespace ASCompletion.Completion
         static public bool IsTextStyleEx(int style)
 		{
             return style == 5 /*word (secondary keywords)*/
-                || style == 11 /*identifier*/ || style == 16 /*word2 (secondary keywords: class name)*/ 
+                || style == 10 /*punctuation*/ || style == 11 /*identifier*/ 
+                || style == 16 /*word2 (secondary keywords: class name)*/ 
                 || style == 19 /*globalclass (primary keywords)*/ || style == 20 /*word3 (add keywords3)*/
                 || style == 21 /*word4 (add keywords4)*/ || style == 22 /*word5 (add keywords5)*/;
 		}
