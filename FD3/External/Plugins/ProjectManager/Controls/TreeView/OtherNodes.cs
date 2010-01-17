b5538fdcbfa86736a8aeecade8e33cf269c69c83
philippe.elsass <philippe.elsass@2354be2f-a62e-0410-afb7-89360a396392>
2010-01-17 21:07:57 +0000

    - little bug when displaying some classpath node names in project tree
    git-svn-id: http://flashdevelop.googlecode.com/svn/trunk@834 2354be2f-a62e-0410-afb7-89360a396392

--- FD3/External/Plugins/ProjectManager/Controls/TreeView/OtherNodes.cs
+++ FD3/External/Plugins/ProjectManager/Controls/TreeView/OtherNodes.cs
@@ -48,7 +48,7 @@ namespace ProjectManager.Controls.TreeView
                 for (int i = parts.Length - 1; i > 0; --i)
                 {
                     String part = parts[i] as String;
-                    if (part != "." && part != ".." && Array.IndexOf(excludes, part) == -1)
+                    if (part != "" && part != "." && part != ".." && Array.IndexOf(excludes, part.ToLower()) == -1)
                     {
                         label = part;
                         break;
