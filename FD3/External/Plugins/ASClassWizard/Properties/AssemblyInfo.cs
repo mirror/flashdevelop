10f2b558b0708a87cf653c1a59689f8f97822a64
meychi <meychi@2354be2f-a62e-0410-afb7-89360a396392>
2010-04-19 06:07:02 +0000

    Assembly info update...
    git-svn-id: http://flashdevelop.googlecode.com/svn/trunk@979 2354be2f-a62e-0410-afb7-89360a396392

--- FD3/External/Plugins/ASClassWizard/Properties/AssemblyInfo.cs
+++ FD3/External/Plugins/ASClassWizard/Properties/AssemblyInfo.cs
@@ -2,33 +2,14 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Information about this assembly is defined by the following
-// attributes.
-//
-// change them to the information which is associated with the assembly
-// you compile.
-
+// Information about this assembly is defined by the following attributes. 
+// Change them to the information which is associated with the assembly you compile.
 [assembly: AssemblyTitle("ASClassWizard")]
-[assembly: AssemblyDescription("Actionscript class wizard plugin For FlashDevelop")]
+[assembly: AssemblyDescription("ActionScript Class Wizard Plugin For FlashDevelop.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("sephiroth.it")]
+[assembly: AssemblyCompany("FlashDevelop.org")]
 [assembly: AssemblyProduct("ASClassWizard")]
-[assembly: AssemblyCopyright("Alessandro Crugnola 2007")]
+[assembly: AssemblyCopyright("FlashDevelop.org 2005-2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// The assembly version has following format :
-//
-// Major.Minor.Build.Revision
-//
-// You can specify all values by your own or you can build default build and revision
-// numbers with the '*' character (the default):
-
-[assembly: AssemblyVersion("1.1.0.*")]
-
-// The following attributes specify the key for the sign of your assembly. See the
-// .NET Framework documentation for more information about signing.
-// This is not required, if you don't want signing let these attributes like they're.
-[assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("")]
-[assembly: GuidAttribute("a2c159c1-7d21-4483-aeb1-38d9fdc4c7f3")]
+[assembly: AssemblyVersion("1.0.0.0")]
