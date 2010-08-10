88c455a268c494c785b86009a983e7ccef619b7a
philippe.elsass <philippe.elsass@2354be2f-a62e-0410-afb7-89360a396392>
2010-08-10 07:44:38 +0000

    git-svn-id: http://flashdevelop.googlecode.com/svn/trunk@1246 2354be2f-a62e-0410-afb7-89360a396392

---
+++ FD3/External/Plugins/CodeRefactor/Commands/ExtractLocalVariableCommand.cs
@@ -0,0 +1,101 @@
+using System;
+using ASCompletion.Context;
+using ASCompletion.Model;
+using PluginCore;
+using PluginCore.Helpers;
+using ScintillaNet;
+using ASCompletion.Completion;
+
+namespace CodeRefactor.Commands
+{
+    class ExtractLocalVariableCommand
+    {
+        private FileModel cFile;
+        private string NewName;
+        private ScintillaControl Sci;
+
+        public ExtractLocalVariableCommand(string newName)
+        {
+            this.NewName = newName;
+        }
+
+        public void Execute()
+        {
+            Sci = PluginBase.MainForm.CurrentDocument.SciControl;
+            Sci.BeginUndoAction();
+            try
+            {
+                IASContext context = ASContext.Context;
+                Int32 pos = Sci.CurrentPos;
+
+                string expression = Sci.SelText.Trim(new char[] { '=', ' ', '\t', '\n', '\r', ';', '.' });
+                expression = expression.TrimEnd(new char[] { '(', '[', '{', '<' });
+                expression = expression.TrimStart(new char[] { ')', ']', '}', '>' });
+
+                cFile = ASContext.Context.CurrentModel;
+                ASFileParser parser = new ASFileParser();
+                parser.ParseSrc(cFile, Sci.Text);
+
+                MemberModel current = cFile.Context.CurrentMember;
+               
+                string characterClass = ScintillaControl.Configuration.GetLanguage(Sci.ConfigurationLanguage).characterclass.Characters;
+
+                int funcBodyStart = ASGenerator.GetBodyStart(current.LineFrom, current.LineTo, Sci);
+                Sci.SetSel(funcBodyStart, Sci.LineEndPosition(current.LineTo));
+                string currentMethodBody = Sci.SelText;
+
+                int lastPos = -1;
+                char prevOrNextChar;
+                Sci.Colourise(0, -1);
+                while (true)
+                {
+                    lastPos = currentMethodBody.IndexOf(expression, lastPos + 1);
+                    if (lastPos > -1)
+                    {
+                        if (lastPos > 0)
+                        {
+                            prevOrNextChar = currentMethodBody[lastPos - 1];
+                            if (characterClass.IndexOf(prevOrNextChar) > -1)
+                            {
+                                continue;
+                            }
+                        }
+                        if (lastPos + expression.Length < currentMethodBody.Length)
+                        {
+                            prevOrNextChar = currentMethodBody[lastPos + expression.Length];
+                            if (characterClass.IndexOf(prevOrNextChar) > -1)
+                            {
+                                continue;
+                            }
+                        }
+                        
+                        int stylemask = (1 << Sci.StyleBits) -1;
+                        int style = Sci.StyleAt(funcBodyStart + lastPos) & stylemask;
+                        if (!ASComplete.IsTextStyle(style))
+                        {
+                            continue;
+                        }
+                        Sci.SetSel(funcBodyStart + lastPos, funcBodyStart + lastPos + expression.Length);
+                        Sci.ReplaceSel(NewName);
+                        currentMethodBody = currentMethodBody.Substring(0, lastPos) + NewName + currentMethodBody.Substring(lastPos + expression.Length);
+                        lastPos += NewName.Length;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                Sci.CurrentPos = funcBodyStart;
+                Sci.SetSel(Sci.CurrentPos, Sci.CurrentPos);
+
+                string snippet = "var " + NewName + ":$(EntryPoint) = " + expression + ";\n$(Boundary)";
+                SnippetHelper.InsertSnippetText(Sci, Sci.CurrentPos, snippet);
+            }
+            finally
+            {
+                Sci.EndUndoAction();
+            }
+        }
+    }
+}
