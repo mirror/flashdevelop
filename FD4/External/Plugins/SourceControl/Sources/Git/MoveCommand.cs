225623498b55e906c5fee968898ec35f98f6690b
philippe.elsass <philippe.elsass@2354be2f-a62e-0410-afb7-89360a396392>
2011-01-22 20:43:26 +0000

    - fixed git operations on empty folders (FD4)
    git-svn-id: http://flashdevelop.googlecode.com/svn/trunk@1716 2354be2f-a62e-0410-afb7-89360a396392

--- FD4/External/Plugins/SourceControl/Sources/Git/MoveCommand.cs
+++ FD4/External/Plugins/SourceControl/Sources/Git/MoveCommand.cs
@@ -11,6 +11,16 @@ namespace SourceControl.Sources.Git
     {
         public MoveCommand(string fromPath, string toPath)
         {
+            // directly move empty dirs
+            if (Directory.Exists(fromPath) && Directory.GetFiles(fromPath).Length == 0)
+            {
+                toPath = Path.Combine(toPath, Path.GetFileName(fromPath));
+                if (Directory.Exists(toPath)) return;
+                try { Directory.Move(fromPath, toPath); }
+                catch (Exception ex) { ErrorManager.ShowInfo(ex.Message); }
+                return;
+            }
+
             string args = String.Format("mv \"{0}\" \"{1}\"", Path.GetFileName(fromPath), toPath);
 
             Run(args, Path.GetDirectoryName(fromPath));
--- FD4/External/Plugins/SourceControl/Sources/Git/RenameCommand.cs
+++ FD4/External/Plugins/SourceControl/Sources/Git/RenameCommand.cs
@@ -11,6 +11,16 @@ namespace SourceControl.Sources.Git
     {
         public RenameCommand(string path, string newName)
         {
+            // directly rename empty dirs
+            if (Directory.Exists(path) && Directory.GetFiles(path).Length == 0)
+            {
+                string newPath = Path.Combine(Path.GetDirectoryName(path), newName);
+                if (Directory.Exists(newPath)) return;
+                try { Directory.Move(path, newPath); }
+                catch (Exception ex) { ErrorManager.ShowInfo(ex.Message); }
+                return;
+            }
+
             string args = String.Format("mv \"{0}\" \"{1}\"", Path.GetFileName(path), newName);
 
             Run(args, Path.GetDirectoryName(path));
