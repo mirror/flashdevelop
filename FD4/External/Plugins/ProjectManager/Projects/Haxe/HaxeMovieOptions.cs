2232066cccda01955487463599c0ccb239d7cce6
philippe.elsass <philippe.elsass@2354be2f-a62e-0410-afb7-89360a396392>
2011-03-18 21:33:18 +0000

    git-svn-id: http://flashdevelop.googlecode.com/svn/trunk@1786 2354be2f-a62e-0410-afb7-89360a396392

--- FD4/External/Plugins/ProjectManager/Projects/Haxe/HaxeMovieOptions.cs
+++ FD4/External/Plugins/ProjectManager/Projects/Haxe/HaxeMovieOptions.cs
@@ -41,7 +41,7 @@ namespace ProjectManager.Projects.Haxe
             switch (platform)
             {
                 case AIR_PLATFORM: return new string[] { "1.5", "2.0", "2.5" };
-                case FLASHPLAYER_PLATFORM: return new string[] { "6.0", "7.0", "8.0", "9.0", "10.0", "10.1", "10.2", "10.3", "11" };
+                case FLASHPLAYER_PLATFORM: return new string[] { "6.0", "7.0", "8.0", "9.0", "10.0", "10.1", "10.2", "10.3", "11.0" };
                 default: return new string[] { "0.0" };
             }
         }
