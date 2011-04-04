a6d5f5f227eef24318020baa2343eec6251fc2ed
philippe.elsass <philippe.elsass@2354be2f-a62e-0410-afb7-89360a396392>
2011-04-04 19:52:41 +0000

    - tooltip wrapping improvement
    git-svn-id: http://flashdevelop.googlecode.com/svn/trunk@1814 2354be2f-a62e-0410-afb7-89360a396392

--- FD4/PluginCore/PluginCore/Controls/WinFormUtils.cs
+++ FD4/PluginCore/PluginCore/Controls/WinFormUtils.cs
@@ -35,8 +35,10 @@ namespace PluginCore.Controls
 			_tempRTB.Visible = false;
 			RichTextBox rtb = useSelfForTest ? richTextBox : _tempRTB;
 			rtb.Rtf = rtf.Substring(0, lastIdx) + @"\par}";
+			
 			rtb.Width = width;
 			rtb.Height = height;
+			rtb.WordWrap = false;
 			rtb.WordWrap = wordWrap;
 
 			if (rtb.ScrollBars != richTextBox.ScrollBars)
@@ -45,7 +47,7 @@ namespace PluginCore.Controls
 			outSize.Height = rtb.GetPositionFromCharIndex(rtb.TextLength).Y;
 
 			lastIdx = -1;
-			int maxW = rtb.GetPositionFromCharIndex(rtb.TextLength).X;
+			int maxW = maxW = rtb.GetPositionFromCharIndex(rtb.TextLength).X;
 			int currW = 0;
 			while (true)
 			{
@@ -58,12 +60,63 @@ namespace PluginCore.Controls
 					maxW = currW;
 			}
 
+			if (wordWrap)
+			{
+				float delta;
+				float maxDelta = -1.0f;
+				int firstLineChars = 0;
+				int firstLineCount = 1;
+				int currLineChars = 0;
+
+				int prevW = -1;
+				int i, l = rtb.Text.Length;
+				for (i = 0; i < l;)
+				{
+					currW = rtb.GetPositionFromCharIndex(i).X;
+					if (currW > maxW)
+						maxW = currW;
+
+					if (maxDelta < 0.0f && prevW > currW)
+					{
+						if (currLineChars > firstLineChars)
+							firstLineChars = currLineChars;
+						currLineChars = 0;
+
+						if (--firstLineCount <= 0)
+							maxDelta = 0.5f * (float)firstLineChars;
+					}
+					prevW = currW;
+
+					if (maxDelta < 0.0f)
+					{
+						++currLineChars;
+						++i;
+						continue;
+					}
+
+					delta = lerp(maxDelta, -0.1f * maxDelta, ((float)currW) / ((float)width));
+
+					if (delta < 1.0f)
+						delta = 1.0f;
+
+					i += (int)delta;
+				}
+			}
+
 			if (useSelfForTest)
 				rtb.Rtf = rtf;
 
 			outSize.Width = maxW;
 			return outSize;
 		}
-		
+
+		// Linear interpolation
+		private static float lerp(float a, float b, float pos/*[0..1]*/)
+		{
+			if (pos < 0.0f) pos = 0.0f;
+			if (pos > 1.0f) pos = 1.0f;
+
+			return a + pos * (b - a);
+		}
 	}
 }
