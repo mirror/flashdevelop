631eeede6aa8a172d3ca796f46eb72f8e3e3861e
philippe.elsass <philippe.elsass@2354be2f-a62e-0410-afb7-89360a396392>
2011-08-13 17:34:34 +0000

    - added a hint to package with captive AIR runtime
    git-svn-id: http://flashdevelop.googlecode.com/svn/trunk@2034 2354be2f-a62e-0410-afb7-89360a396392

--- FD4/FlashDevelop/Bin/Debug/Projects/170 ActionScript 3 - AIR Flex 3 Projector/PackageApp.bat
+++ FD4/FlashDevelop/Bin/Debug/Projects/170 ActionScript 3 - AIR Flex 3 Projector/PackageApp.bat
@@ -4,6 +4,7 @@ call bat\SetupSDK.bat
 call bat\SetupApplication.bat
 
 set AIR_TARGET=
+::set AIR_TARGET=-captive-runtime
 set OPTIONS=
 call bat\Packager.bat
 
--- FD4/FlashDevelop/Bin/Debug/Projects/195 ActionScript 3 - Flex Android App/PackageApp.bat
+++ FD4/FlashDevelop/Bin/Debug/Projects/195 ActionScript 3 - Flex Android App/PackageApp.bat
@@ -4,6 +4,7 @@ call bat\SetupSDK.bat
 call bat\SetupApplication.bat
 
 set APK_TARGET=
+::set APK_TARGET=-captive-runtime
 set OPTIONS=
 call bat\Packager.bat
 
