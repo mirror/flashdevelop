aae5cebdfc3cfe203df6c0fa42783159c9012bb2
philippe.elsass <philippe.elsass@2354be2f-a62e-0410-afb7-89360a396392>
2011-11-11 09:15:58 +0000

    - FP11.1, AIR3.1
    git-svn-id: http://flashdevelop.googlecode.com/svn/trunk@2156 2354be2f-a62e-0410-afb7-89360a396392

--- FD4/External/Plugins/ProjectManager/Projects/AS3/AS3MovieOptions.cs
+++ FD4/External/Plugins/ProjectManager/Projects/AS3/AS3MovieOptions.cs
@@ -32,9 +32,9 @@ namespace ProjectManager.Projects.AS3
             switch (platform)
             {
                 case CUSTOM_PLATFORM: return new string[] { "0.0" };
-                case AIR_MOBILE_PLATFORM: return new string[] { "2.5", "2.6", "2.7", "3.0" };
-                case AIR_PLATFORM: return new string[] { "1.5", "2.0", "2.5", "2.6", "2.7", "3.0" };
-                default: return new string[] { "9.0", "10.0", "10.1", "10.2", "10.3", "11.0" };
+                case AIR_MOBILE_PLATFORM: return new string[] { "2.5", "2.6", "2.7", "3.0", "3.1" };
+                case AIR_PLATFORM: return new string[] { "1.5", "2.0", "2.5", "2.6", "2.7", "3.0", "3.1" };
+                default: return new string[] { "9.0", "10.0", "10.1", "10.2", "10.3", "11.0", "11.1" };
             }
         }
 
