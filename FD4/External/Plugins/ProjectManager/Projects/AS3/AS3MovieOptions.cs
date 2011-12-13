1009807b972f5fa4731dbbb66c57010f7e184e29
philippe.elsass <philippe.elsass@2354be2f-a62e-0410-afb7-89360a396392>
2011-12-13 12:44:45 +0000

    - swf-version fix
    git-svn-id: http://flashdevelop.googlecode.com/svn/trunk@2175 2354be2f-a62e-0410-afb7-89360a396392

--- FD4/External/Plugins/ProjectManager/Projects/AS3/AS3MovieOptions.cs
+++ FD4/External/Plugins/ProjectManager/Projects/AS3/AS3MovieOptions.cs
@@ -34,7 +34,7 @@ namespace ProjectManager.Projects.AS3
                 case CUSTOM_PLATFORM: return new string[] { "0.0" };
                 case AIR_MOBILE_PLATFORM: return new string[] { "2.5", "2.6", "2.7", "3.0", "3.1", "3.2" };
                 case AIR_PLATFORM: return new string[] { "1.5", "2.0", "2.5", "2.6", "2.7", "3.0", "3.1", "3.2" };
-                default: return new string[] { "9.0", "10.0", "10.1", "10.2", "10.3", "11.0", "11.1", "11.2", "12" };
+                default: return new string[] { "9.0", "10.0", "10.1", "10.2", "10.3", "11.0", "11.1", "11.2" };
             }
         }
 
@@ -73,7 +73,7 @@ namespace ProjectManager.Projects.AS3
             if (Platform != FLASHPLAYER_PLATFORM) return null;
             int index = Array.IndexOf(TargetVersions(FLASHPLAYER_PLATFORM), Version);
             if (index < 0) return null;
-            string[] versions = new string[] { "9", "10", "10", "11", "12", "13", "13", "14", "15" };
+            string[] versions = new string[] { "9", "10", "10", "11", "12", "13", "14", "15" };
             return versions[index];
         }
     }
