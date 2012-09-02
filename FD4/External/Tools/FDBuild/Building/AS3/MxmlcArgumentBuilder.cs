c559de3332d5389e07eb8a3a4342b40fc7f015eb
philippe.elsass <philippe.elsass@2354be2f-a62e-0410-afb7-89360a396392>
2012-09-02 15:05:36 +0000

    - fixed parameter override detection
    git-svn-id: http://flashdevelop.googlecode.com/svn/trunk@2392 2354be2f-a62e-0410-afb7-89360a396392

--- FD4/External/Tools/FDBuild/Building/AS3/MxmlcArgumentBuilder.cs
+++ FD4/External/Tools/FDBuild/Building/AS3/MxmlcArgumentBuilder.cs
@@ -47,8 +47,8 @@ namespace ProjectManager.Building.AS3
             if (options.Additional != null)
             {
                 string all = String.Join(" ", options.Additional);
-                if (all.IndexOf("configname=") > 0) hasConfig = true;
-                if (all.IndexOf("swf-version=") > 0) hasVersion = true;
+                if (all.IndexOf("configname") > 0) hasConfig = true;
+                if (all.IndexOf("swf-version") > 0) hasVersion = true;
             }
 
             if (!hasConfig)
