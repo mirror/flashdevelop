86fb7fd131d0727baf07b941e80555ddb044f719
meychi <meychi@2354be2f-a62e-0410-afb7-89360a396392>
2013-03-27 07:28:29 +0000

    More config tweaks...
    git-svn-id: http://flashdevelop.googlecode.com/svn/trunk@2616 2354be2f-a62e-0410-afb7-89360a396392

--- FD4/External/Plugins/LoomContext/Properties/AssemblyInfo.cs
+++ FD4/External/Plugins/LoomContext/Properties/AssemblyInfo.cs
@@ -2,35 +2,14 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// Information about this assembly is defined by the following attributes. 
+// Change them to the information which is associated with the assembly you compile.
 [assembly: AssemblyTitle("LoomContext")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("LoomContext Plugin For ASCompletion In FlashDevelop.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("FlashDevelop.org")]
 [assembly: AssemblyProduct("LoomContext")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("FlashDevelop.org 2005-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("247bcdb4-a456-4072-9bf3-1c71143d25d2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
