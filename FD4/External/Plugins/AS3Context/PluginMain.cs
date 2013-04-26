3e4deb36af09934b2b2997d540ee2d81b7f7ec1c
philippe.elsass <philippe.elsass@2354be2f-a62e-0410-afb7-89360a396392>
2013-04-26 21:06:47 +0000

    - get class model from SWF was disabled
    git-svn-id: http://flashdevelop.googlecode.com/svn/trunk@2658 2354be2f-a62e-0410-afb7-89360a396392

--- FD4/External/Plugins/AS3Context/PluginMain.cs
+++ FD4/External/Plugins/AS3Context/PluginMain.cs
@@ -144,12 +144,12 @@ namespace AS3Context
                     case EventType.Command:
                         DataEvent de = e as DataEvent;
                         string action = de.Action;
-                        /*if (action == "ProjectManager.OpenVirtualFile")
+                        if (action == "ProjectManager.OpenVirtualFile")
                         {
-                            if (PluginBase.CurrentProject != null && PluginBase.CurrentProject.Language == "AS3")
+                            if (PluginBase.CurrentProject != null && PluginBase.CurrentProject.Language == "as3")
                                 e.Handled = OpenVirtualFileModel(de.Data as String);
                         }
-                        else*/ if (!(settingObject as AS3Settings).DisableFDB && action == "AS3Context.StartDebugger")
+                        else if (!(settingObject as AS3Settings).DisableFDB && action == "AS3Context.StartDebugger")
                         {
                             string workDir = (PluginBase.CurrentProject != null)
                                 ? Path.GetDirectoryName(PluginBase.CurrentProject.ProjectPath)
